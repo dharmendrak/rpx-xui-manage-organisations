c5d6766130e02e69a4cd5c148cc2fb0364b9e204
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-10-16 18:08:58 +0100

    EUI-639 tests #!"£$%^&*

--- src/users/containers/user-details/user-details.component.spec.ts
+++ src/users/containers/user-details/user-details.component.spec.ts
@@ -8,116 +8,31 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { of, BehaviorSubject, Observable } from 'rxjs';
 import { UsersService } from '../../services';
 
-
-
-const userStateDummy = {
-    invitedUsers: {
-        userList: [],
-        loaded: false,
-        loading: false
-    },
-    inviteUser: {
-        inviteUserFormData: {},
-        errorMessages: {},
-        isFormValid: true,
-        errorHeader: '',
-        isUserConfirmed: false
-    }
-};
-
-const routerStateDummy = {
-    routerReducer: {
-        state: {
-            url: '/users',
-            queryParams: {},
-            params: {}
-        },
-        navigationId: 1
-    },
-    appState: {
-        allNavItems: {
-            'pui-organisation-manager': {
-                text: 'Organisation',
-                href: '/organisation',
-                active: false,
-                orderId: 1
-            },
-            'pui-user-manager': {
-                text: 'Users',
-                href: '/users',
-                active: false,
-                orderId: 2
-            },
-            'pui-finance-manager': {
-                text: 'Fee Accounts',
-                href: '/fee-accounts',
-                active: false,
-                orderId: 3
-            }
-        },
-        pageTitle: 'Users - Manage organisation',
-        userNav: {
-            label: 'Account navigation',
-            items: [
-                {
-                    text: 'Sign out',
-                    emit: 'sign-out'
-                }
-            ]
-        },
-        navItems: [
-            {
-                text: 'Organisation',
-                href: '/organisation',
-                active: false,
-                orderId: 1
-            },
-            {
-                text: 'Users',
-                href: '/users',
-                active: false,
-                orderId: 2
-            },
-            {
-                text: 'Fee Accounts',
-                href: '/fee-accounts',
-                active: false,
-                orderId: 3
-            }
-        ],
-        headerTitle: {
-            regOrg: {
-                name: 'Register to manage civil and family law cases',
-                url: '/register-org/register/'
-            },
-            manageOrg: {
-                name: 'Manage organisation details for civil and family law cases',
-                url: '/'
-            }
-        },
-        jurisdictions: []
-    }
-};
-
 const usersServiceMock = jasmine.createSpyObj('UserService', [
     'getListOfUsers'
 ]);
 
 export class MockedStore {
-    private state: BehaviorSubject<any> = new BehaviorSubject(undefined);
 
-    setState(data) {
-        this.state.next(data);
+    select(...params) {
+        return params[0].name;
     }
 
     pipe(selector?: any): Observable<any> {
-        return this.state.asObservable();
+        let response: any = false;
+        console.log(selector.arguments[0].name);
+        // if (selector.includes('select(fromRoot.getRouterState)')) {
+        //     response = { state: { params: { userId: 'user' } } };
+        // } else {
+        //     response = false;
+        // }
+        return of(response);
     }
 
     dispatch(action: any) { }
 }
 
-fdescribe('User Details Component', () => {
+describe('User Details Component', () => {
 
     let fixture: ComponentFixture<UserDetailsComponent>;
     let component: UserDetailsComponent;
@@ -147,25 +62,20 @@ fdescribe('User Details Component', () => {
         component = fixture.componentInstance;
     }));
 
-    it('should have a component', inject([Store, Store], (
-        testUserStore: MockedStore,
-        testRouterStore: MockedStore
-    ) => {
-        testUserStore.setState(userStateDummy);
-        testRouterStore.setState(routerStateDummy);
+    it('should have a component', () => {
         component.user$ = of({
-            userIdentifier: 'cfeba78e-ff81-49d5-8a65-55fa2a9c2424',
-            firstName: 'Philip',
-            lastName: 'james',
-            email: 'philip.aberneithy@gmail.com',
+            userIdentifier: 'cfeba78e',
+            firstName: 'John',
+            lastName: 'James',
+            email: 'aaaa@gmail.com',
             idamStatus: 'PENDING',
             idamStatusCode: ' ',
-            idamMessage: '19 No call made to SIDAM to get the user roles as user status is ‘Pending’',
-            fullName: 'Philip james',
+            idamMessage: 'Doh',
+            fullName: 'john james',
             selected: false,
             status: 'Active'
         });
         fixture.detectChanges();
         expect(component).toBeTruthy();
-    }));
+    });
 });
