442c3ddfe4d7a117a236cb65153f56b43c7bea90
Philip <philip.aberneithy@gmail.com>
2020-04-28 16:59:59 +0100

    Error page copy, and we take the User back to Edit User Permissions if there is an error.

--- src/users/containers/edit-user-permissions-failure/edit-user-permissions-failure.component.ts
+++ src/users/containers/edit-user-permissions-failure/edit-user-permissions-failure.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { ErrorMessage, GlobalError } from 'src/app/store/reducers/app.reducer';
@@ -10,8 +11,12 @@ import {EditUserFailureReset} from '../../store/actions';
     templateUrl: './edit-user-permissions-failure.component.html'
 })
 export class EditUserPermissionsFailureComponent implements OnInit {
+
+    public userId: string;
+
     constructor(
       private readonly userStore: Store<fromStore.UserState>,
+      private readonly route: ActivatedRoute,
     ) {
     }
     // public ngOnDestroy(): void {
@@ -21,17 +26,15 @@ export class EditUserPermissionsFailureComponent implements OnInit {
       console.log('EditUserFailureComponent');
 
       this.userStore.dispatch(new fromStore.EditUserFailureReset());
-        // this.currentError = {
-        //     errors: [{bodyText: 'Try again later.', urlText: null, url: null, newTab: null}],
-        //     header: 'Sorry, there is a problem with the service'
-        // };
-        // this.store.pipe(select(fromAppStore.getCurrentError))
-        // .subscribe(error => {
-        //     if (error) {
-        //         this.currentError = error;
-        //     }
-        // });
+
+      this.route.paramMap.subscribe(params => {
+        this.userId = params.get('userId');
+      });
+
+      console.log(this.userId);
     }
+
+    public getEditUserPermissionsLink = (userId: string) => `/users/user/${userId}`;
 
     // public showErrorLinkWithNewTab(newTab?: boolean): string {
     //     return (newTab !== null && newTab === true) ? '_blank' : '_self';
